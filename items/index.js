0848e12f7a944fc4738db7ea8cea0d8525b87a71
Mimi Pham <phammimi465@gmail.com>
2020-12-22 16:55:36 -0500

    index.js

---
+++ items/index.js
@@ -0,0 +1,5 @@
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/Box.png"},
+{"id": "item2", "fname":"item2.md", "specialImage":"/img/Coat.png"},
+{"id": "item3", "fname":"item3.md", "specialImage":""},
+{"id": "item4", "fname":"item4.md", "specialImage":""},
+{"id": "item5", "fname":"item5.md", "specialImage":""}];
